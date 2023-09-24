f30ceff0bb66da95b8c071c7f5d976135e2abddd
ZeroXClem <131801011+ZeroXClem@users.noreply.github.com>
2023-09-24 15:06:11 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,76 @@
+# Next JS x window.ai
+
+This is a simple Next JS application that demonstrates how to integrate [window.ai](windowai.io) to build a chatbot interface. The app provides a user-friendly chat interface for users to communicate with an AI assistant.
+
+## Features
+
+- User-friendly chat interface
+- AI assistant powered by [window.ai](windowai.io)
+- Automatically scrolls to the most recent message
+- Loading state while waiting for the AI to respond
+- Responsive design
+
+Built by [Yanni](https://twitter.com/YKouloumbis) & [Nolan](https://twitter.com/nolangclement).
+
+## Installation
+
+To get started with this app, follow these steps:
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/YanniKouloumbis/next-js-window-ai/
+```
+
+2. Change directory to the project folder:
+
+```bash
+cd next-js-window-ai
+```
+
+3. Install the required dependencies:
+
+```bash
+pnpm install
+```
+
+4. Start the development server:
+
+```bash
+pnpm dev
+```
+
+5. Visit `http://localhost:3000` in your browser to see the app in action.
+
+## Usage
+
+1. Type a message in the input field at the bottom of the chat window.
+2. Press "Send" or hit "Enter" to send the message.
+3. The AI assistant will respond with a message after processing your input.
+4. Continue the conversation by sending more messages.
+
+## Deployment
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/YanniKouloumbis/next-js-window-ai)
+
+
+## Customization
+
+You can customize the app by modifying the `pages/index.js` file. Here are some ideas for customization:
+
+- Change the initial system message to set a different context for the AI assistant.
+- Adjust the streaming options like `temperature` and `maxTokens` to control the AI's response.
+- Update the UI to match your branding by changing colors, fonts, and other styles.
+
+## Contributing
+
+Feel free to contribute to this project by submitting a pull request, reporting bugs, or suggesting new features.
+
+## License
+
+This project is licensed under the MIT License. Use it for anything!
+
+## Acknowledgments
+
+- The `window.ai` library for providing the AI assistant functionality.
+- The Next.js framework for simplifying the development of React applications.
---
+++ pages/index.tsx
@@ -0,0 +1,154 @@
+import React, { useState, useEffect, useRef } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import InstallationToast from "@/components/toast";
+import { ChatMessage, MessageOutput, WindowAI, getWindowAI } from "window.ai";
+
+const App: React.FC = () => {
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const aiRef = useRef<WindowAI | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      aiRef.current = await getWindowAI();
+      if (aiRef.current) {
+        toast.success("window.ai detected!", {
+          id: "window-ai-detected",
+        });
+      } else {
+        toast.custom(<InstallationToast />, {
+          id: "window-ai-not-detected",
+        });
+      }
+    };
+    init();
+  }, []);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const handleSendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!inputValue) return;
+    if (!aiRef.current) {
+      toast.custom(<InstallationToast />, {
+        id: "window-ai-not-detected",
+      });
+      return;
+    }
+
+    const userMessage: ChatMessage = { role: "user", content: inputValue };
+    //creates a local variable to handle streaming state
+    let updatedMessages: ChatMessage[] = [...messages, userMessage];
+    setMessages((prevMessages) => [...prevMessages, userMessage]);
+    setLoading(true);
+    setInputValue("");
+
+    //streaming options settings for window.ai
+    const streamingOptions = {
+      temperature: 0.7,
+      maxTokens: 1000,
+      onStreamResult: (result: MessageOutput | null, error: string | null) => {
+        setLoading(false);
+        if (error) {
+          toast.error("window.ai streaming completion failed.");
+          return;
+        } else if (result) {
+          console.log(result);
+          const lastMessage = updatedMessages[updatedMessages.length - 1];
+          // if the last message is from a user, init a new message
+          if (lastMessage.role === "user") {
+            updatedMessages = [
+              ...updatedMessages,
+              {
+                role: "assistant",
+                content: result.message.content,
+              },
+            ];
+          } else {
+            // if the last message is from the assistant, append the streaming result to the last message
+            updatedMessages = updatedMessages.map((message, index) => {
+              if (index === updatedMessages.length - 1) {
+                return {
+                  ...message,
+                  content: message.content + result.message.content,
+                };
+              }
+              return message;
+            });
+          }
+          setMessages(updatedMessages);
+        }
+      },
+    };
+    //function call to window.ai to generate text, using our streaming options
+    try {
+      await aiRef.current.generateText(
+        {
+          messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            ...updatedMessages,
+          ],
+        },
+        streamingOptions
+      );
+    } catch (e) {
+      toast.error("window.ai generation completion failed.");
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full sm:w-3/4 lg:w-1/2 xl:w-1/2 bg-white shadow-lg rounded-lg p-6">
+        <h1 className="text-3xl font-bold mb-4">Next JS x window.ai</h1>
+        <div className="overflow-y-auto h-96 mb-4">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`mb-2 ${message.role === "user" ? "text-right" : ""}`}
+            >
+              <span
+                className={`inline-block p-2 rounded-lg text-left ${
+                  message.role === "user"
+                    ? "bg-blue-500 text-white"
+                    : "bg-gray-300 text-black"
+                }`}
+              >
+                {message.content}
+              </span>
+            </div>
+          ))}
+          <div ref={messagesEndRef}></div>
+        </div>
+        <form onSubmit={handleSendMessage} className="flex">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="flex-grow border border-gray-300 rounded-lg p-2 text-sm focus:outline-none focus:border-blue-500"
+          />
+          <button
+            type="submit"
+            disabled={loading}
+            className={`ml-2 bg-blue-500 text-white px-4 py-2 rounded-lg text-sm font-semibold ${
+              loading ? "opacity-50" : ""
+            }`}
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
